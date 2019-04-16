7365b017602a4e89098bab6d6caf4b9af80b17e1
anis brahmi <anisbrahmi840@gmail.com>
2019-04-17 00:30:53 +0100

    terminé

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\Table(name="`user`")
+ * @UniqueEntity("email")
+ *
  */
 class User extends BaseUser
 {
